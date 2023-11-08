d6c88c34b4c5ec585e3582315a95b8e9889ad12d
root <root@ubuntuos.myguest.virtualbox.org>
2023-11-08 16:40:40 +0300

    final version

--- rest/app/sqlmethods.go
+++ rest/app/sqlmethods.go
@@ -1,13 +1,11 @@
 package app
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
-	"path/filepath"
 	model "rest/app/model"
 )
 
@@ -16,45 +14,34 @@ type SqlDatabase struct {
 }
 
 func (s *SqlDatabase) connect() error {
-	wd, err := os.Getwd()
-	if err != nil {
-		return fmt.Errorf("ошибка получения текущей директории")
-	}
-	envPath := filepath.Join(wd, "..", "rest", ".env")
-
-	err = godotenv.Load(envPath)
-	if err != nil {
-		return fmt.Errorf("ошибка получения environment файла")
-	}
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"))
+    os.Getenv("DB_HOST"),
+    os.Getenv("DB_USER"),
+    os.Getenv("DB_PASSWORD"),
+    os.Getenv("DB_NAME"),
+    os.Getenv("DB_PORT"))
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	s.dbSql = db
-	if err != nil {
-		fmt.Errorf("ошибка при подключении к БД")
-	}
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+  s.dbSql = db
+  if err != nil {
+    return fmt.Errorf("ошибка при подключении к БД")
+  }
 
-	err = s.dbSql.AutoMigrate(&model.SqlModel{})
-	if err != nil {
-		return fmt.Errorf("ошибка при миграции")
-	}
+  err = s.dbSql.AutoMigrate(&model.SqlModel{})
+  if err != nil {
+    return fmt.Errorf("ошибка при миграции")
+  }
 
-	return nil
+  return nil
 }
 
-// get fixme: переделать получение поля из базы данных !
-func (s *SqlDatabase) getFullUrl(url string) (string, error) {
+func (s *SqlDatabase) getFullUrl(url string) (string, error) {    
 	var urlRequest model.SqlModel
-	err := s.dbSql.Find(&urlRequest, "short_url = ?", url).Error
-	if err != nil {
-		return "", fmt.Errorf("сокращенный url не найден")
-	}
-	return urlRequest.FullUrl, nil
+    	err := s.dbSql.Find(&urlRequest, "short_url = ?", url).Error    
+    	if err != nil {
+       		return "", fmt.Errorf("сокращенный url не найден")    
+       	}
+    	return urlRequest.FullUrl, nil
 }
 
 func (s *SqlDatabase) create(shortUrl, fullUrl string) error {
@@ -62,17 +49,15 @@ func (s *SqlDatabase) create(shortUrl, fullUrl string) error {
 		return fmt.Errorf("переданы некорректные url")
 	}
 
-	newUrl := model.SqlModel{
+	var existingUrl model.SqlModel
+	result := s.dbSql.Where("short_url = ?", shortUrl).FirstOrCreate(&existingUrl, model.SqlModel{
 		ShortUrl: shortUrl,
 		FullUrl:  fullUrl,
+	})
+	if result.Error != nil {
+		return fmt.Errorf("ошибка при создании/поиске записи")
 	}
-
-	// todo: переписать логику добавления нового url-адреса (не добавляется новый (уникальный) url-адрес.
-	result := s.dbSql.First(&newUrl, fullUrl)
-	ok := !errors.Is(result.Error, gorm.ErrRecordNotFound)
-	if ok {
-		s.dbSql.Create(&newUrl)
-		return nil
-	}
-	return fmt.Errorf("данное значение уже существует")
+	
+	logger.Println("val = ", existingUrl.FullUrl)
+	return nil
 }
